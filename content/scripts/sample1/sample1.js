7f20327bb2ee65b0549bbc56fa0d95df274eaac0
sekaiamber <a3824036@126.com>
2015-07-11 12:46:04 +0800

    [feature]. add 7 content. add script. change banner style.

--- content/scripts/sample1/sample1.js
+++ content/scripts/sample1/sample1.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     headerInit();
     parallaxInit();
+    transInit();
 });
 
 function headerInit() {
@@ -16,4 +17,11 @@ function parallaxInit() {
         top *= 0.4;
         $parallax.css('transform', 'translateY(-' + top + 'px)')
     });
+};
+
+function transInit() {
+    $(".top .-trans-bt").click(function(){
+        var top = $(".-content-2").first().position().top;
+        $('body,html').animate({ scrollTop: top }, 1200);
+    })
 };
